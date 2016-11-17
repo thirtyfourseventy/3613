85541dfe355d2c2614c0f8d695a150eb108915e0
Edward Lambke <edward.lambke@elasticpath.com>
2016-11-17 11:27:10 -0800

    better login

--- A00892244_assignment2/src/a00892244/assignment2/filters/LoginFilter.java
+++ A00892244_assignment2/src/a00892244/assignment2/filters/LoginFilter.java
@@ -12,93 +12,49 @@ import javax.servlet.http.*;
 public class LoginFilter implements Filter {
 	private String PASSWORD = "java3613";
 
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws ServletException, IOException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws ServletException, IOException {
 
-	
 		System.out.println("Filter this!");
-	
+
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession();
 
-		String redirect = "login";
-		
+		boolean authenticated = false;
+
 		if (session.getAttribute("authenticated") != null) {
-			redirect = "proceed";
-		}
-		
-		if (session.getAttribute("authenticated") == null) {		
-			if (request.getParameter("password") != null) {
-				System.out.println("password = " + request.getParameter("password").toString());
-				if (request.getParameter("password").toString().equals(PASSWORD)){
-					session.setAttribute("authenticated", true);
-					redirect = "table";
-				}				
-			}
-			
-		}
-			
-		if (redirect.equals("table")) {
-			System.out.println("proceed to table!");
-			RequestDispatcher dispatcher = req.getSession().getServletContext().getRequestDispatcher("/assignment2");
-			dispatcher.forward(request, response);
+			authenticated = true;
 		}
 
-		else if (redirect.equals("login")){
+		if (session.getAttribute("authenticated") == null) {
+			if (request.getParameter("password") != null) {
+				System.out.println("password = " + request.getParameter("password").toString());
+				if (request.getParameter("password").toString().equals(PASSWORD)) {
+					session.setAttribute("authenticated", true);
+					authenticated = true;
+				}
+			}
+		}
+
+		if (!authenticated) {
 			System.out.println("Back to login!");
 			RequestDispatcher dispatcher = req.getSession().getServletContext().getRequestDispatcher("/login.jsp");
 			dispatcher.forward(request, response);
-		}
-		else  {
+		} else {
 			chain.doFilter(request, response);
 		}
 	}
 
-	/**
-	 * Create a table of banned sites based on initialization parameters .
-	 */
-	public void init(FilterConfig config) throws ServletException {
-		
-//		bannedSiteTable = new HashSet<String>();
-//		String bannedSites = config.getInitParameter("bannedSites");
-//		if (bannedSites == null) {
-//			return;
-//		}
-//		// Split using one or more white spaces
-//		String[] sites = bannedSites.split("\\s++");
-//		for (String bannedSite : sites) {
-//			bannedSiteTable.add(bannedSite);
-//			System.out.println("Banned " + bannedSite);
-//		}
-	}
 
 	public void destroy() {
 	}
 
-	private String getReferringHost(String refererringURLString) {
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
 		
-		try {
-			URL referringURL = new URL(refererringURLString);
-			return (referringURL.getHost());
-		} catch (MalformedURLException mue) { // Malformed or null
-			return (null);
-		}
 	}
 
-	// Replacement response that is returned to users
-	// who are from or referred here by a banned site.
 
-	private void showWarning(ServletResponse response, String bannedSite)
-			throws ServletException, IOException {
-		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		String docType = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 "
-				+ "Transitional//EN\">\n";
-		out.println(docType + "<HTML>\n"
-				+ "<HEAD><TITLE>Access Prohibited</TITLE></HEAD>\n"
-				+ "<BODY BGCOLOR=\"WHITE\">\n" + "<H1>Access Prohibited</H1>\n"
-				+ "Sorry, access from or via " + bannedSite + "\n"
-				+ "is not allowed.\n" + "</BODY></HTML>");
-	}
 }
